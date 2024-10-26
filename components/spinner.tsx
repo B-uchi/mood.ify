a263ecc28a2fb139d9fd7cd2cecd744a23274136
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-10-26 11:06:22 +0100

    added spinner component

---
+++ components/spinner.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    /* From Uiverse.io by barisdogansutcu */
+    <div className="">
+      <svg className="spinnerS" viewBox="25 25 50 50">
+        <circle className="spinnerC" r="10" cy="50" cx="50"></circle>
+      </svg>
+    </div>
+  );
+};
+
+export default Spinner;
