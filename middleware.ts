aa5730986e9586c96f98633538eb23fcec06a1b7
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-11-11 15:15:51 +0100

    handle api auth

---
+++ middleware.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+const isValidOrigin = (request: NextRequest): boolean => {
+  const origin = request.headers.get("referer");
+  console.log(origin);
+  const allowedOrigins = [
+    process.env.NEXT_PUBLIC_FRONTEND_URL,
+    "http://localhost:3000/",
+    "http://localhost:3000/playlist",
+  ].filter(Boolean);
+
+  return origin ? allowedOrigins.includes(origin) : false;
+};
+
+const isValidApiKey = (request: NextRequest): boolean => {
+  const apiKey = request.headers.get("x-api-key");
+  const validApiKey = process.env.NEXT_PUBLIC_API_KEY_SECRET;
+
+  return apiKey === validApiKey;
+};
+
+export function middleware(request: NextRequest) {
+  if (isValidOrigin(request) && isValidApiKey(request)) {
+    return NextResponse.next();
+  } else {
+    return NextResponse.json(
+      { message: "Unknown origin or incorrect api key" },
+      { status: 401 }
+    );
+  }
+}
+
+export const config = {
+  matcher: [
+    "/api/spotify-auth",
+    "/api/spotify-auth/token",
+    "/api/generate-playlist",
+  ],
+};
