9eb43a8f314441ce13b49dff1c9bd102789607e6
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-11-12 10:26:11 +0100

    ADD: added favicon & site metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,26 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Mood.ify",
   description: "Create magical playlists from your feelings.",
-};
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+    other: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        url: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        url: '/favicon-16x16.png',
+      }
+    ]
+  },
+  manifest: '/site.webmanifest'
+}
 
 export default function RootLayout({
   children,
