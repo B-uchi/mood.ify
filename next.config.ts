cce75b252c2139ccfa5c368bfe56a42b2f843b3a
Umeano Ibuchukwu <ibumeano@gmail.com>
2024-11-12 12:57:31 +0100

    ADD: config for img optimization

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'i.scdn.co',
+        protocol: 'https',
+        port: '',
+        pathname: '/image/**' 
+      }
+    ],
+    deviceSizes: [640, 750, 828, 1080, 1200],
+    imageSizes: [64, 96, 128, 256, 384],
+  }
 };
 
 export default nextConfig;
